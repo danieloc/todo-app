3f0876e69ad446643e294dc4c79ae7808d7e0fa2
Daniel O'Connor <daniel.o.c.03@gmail.com>
2022-01-21 21:42:01 +0000

    Add ddl

---
+++ db/sql/V002__Task_List.sql
@@ -0,0 +1,22 @@
+CREATE TYPE CompletionStatus as ENUM ('TODO', 'DONE');
+
+CREATE TABLE todo.task_lists (
+  task_list_id BIGINT NOT NULL,
+  title INT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  PRIMARY KEY (task_list_id)
+);
+
+
+CREATE TABLE todo.task (
+  task_id BIGINT NOT NULL,
+  task_list_id BIGINT NOT NULL,
+  status CompletionStatus NOT NULL,
+  title INT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  PRIMARY KEY (task_id)
+);
+
+
+ALTER TABLE todo.task_lists
+ADD CONSTRAINT task_list_id_fk FOREIGN KEY (task_list_id) REFERENCES todo.task_lists (task_list_id);
